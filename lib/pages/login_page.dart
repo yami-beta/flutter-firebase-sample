8dadef141bf1037a5661cab2024456d792ec7835
yami-beta <yami-beta@users.noreply.github.com>
2021-08-20 10:59:29 +0900

    feat: ホームとログイン画面を追加
    2画面を行き来出来るところまで実装
    
    初期生成されるカウンター周りは削除
    ただテストコードは後で参考にするかもしれないので残している

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: const Text('Back'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
